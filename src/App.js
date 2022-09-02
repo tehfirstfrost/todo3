2d57e1a89bcc96e0690a6995b2b84c39f172e484
tehfirstfrost <tehfirstfrost@gmail.com>
2022-09-01 22:59:16 -0500

    Refactor

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TodoContainer} from './components';
+import { NotFound, Home } from './pages';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from 'react'
+
+
+
+
+const App = () => {
+  return (
+    <div className='app'>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/main" element={<TodoContainer />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
+
